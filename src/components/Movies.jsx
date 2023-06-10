a43255085f30a5bd4c9081c10ce885a3ece7a422
Nikias211 <nikitakrut.2110@gmail.com " git config --global user.email nikitakrut.2110@gmail.com>
2023-06-10 11:42:24 +0300

    s

---
+++ src/components/Movies.jsx
@@ -0,0 +1,24 @@
+import {Movie} from './Movie';
+import poro from './pick.png'
+
+function Movies (props){
+    const { movies = [] }= props;
+
+    return(
+        // eslint-disable-next-line
+        <div className="movies">
+           {movies.length ? (
+               movies.map((movie)=> <Movie key={movie.imdbID} {...movie} />)
+           ): (
+            <div className='notfound'>
+               <h4>Not found</h4>
+                <div className='pick'>
+                    <img src={poro}/>
+                </div>
+            </div>
+           )}
+        </div>
+   );
+}
+
+export {Movies};
